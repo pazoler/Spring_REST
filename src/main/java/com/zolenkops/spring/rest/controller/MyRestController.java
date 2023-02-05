284126ffddd66cbf38936e3daefebbf93bc125c9
pazoler <56042404+pazoler@users.noreply.github.com>
2023-02-05 17:33:11 +0300

    put, post, delete API

--- src/main/java/com/zolenkops/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zolenkops/spring/rest/controller/MyRestController.java
@@ -36,7 +36,28 @@ public class MyRestController {
 
         return employee;
     }
+    @PostMapping("/employees")
+    public Employee addNewEmployee (@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 
+   @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee==null) {
+            throw new NoSuchEmployeeException("There is no employee " +
+                    "with id = " + id + " in DB");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
+    }
 
 }
